492118fd6262d32755902e07f67f961de7d11b11
mikz <mikias@outlook.com>
2016-10-06 05:12:59 -0400

    dev/null

--- config_mgt/files/installer.rb
+++ config_mgt/files/installer.rb
@@ -16,7 +16,7 @@ restart = "service apache2 restart"
 #scheduler.every '10s' do
 
 data_hash.each do |key,value|# hash...
-        
+
 	if(value['type'].eql?('package') && value['version'].nil?)
 	  system( "dpkg -l | grep -q #{key}" ) #quiet grep
 	  if($?.exitstatus > 0)
@@ -26,7 +26,7 @@ data_hash.each do |key,value|# hash...
 	  system ""#reset exitcode
          end #end  #php,apache install
     	if(value['type'].eql?('service'))
-	  system("#{value['type']} #{value['name']} status")
+	  system("#{value['type']} #{value['name']} status > /dev/null")
 	  if($?.exitstatus > 0)
 	    puts "#{key}....not installed exitcode =#{$?.exitstatus}"
 	    system "#{value['type']} #{value['name']} #{key} > /dev/null"
@@ -38,10 +38,10 @@ data_hash.each do |key,value|# hash...
 	  File.file?(path_name)
 	  dir_name = File.dirname(path_name)
 	  Dir.chdir(dir_name)
-	  if(File.file?(path_name)) 
+	  if(File.file?(path_name))
 	    read_file = File.read(path_name) #file is read
 	    puts read_file
-	  else 
+	  else
 	   system "touch #{key} > /dev/null" #create the php by force
 	   read_file = File.read(path_name) #file is read
 	  end
@@ -51,7 +51,7 @@ data_hash.each do |key,value|# hash...
 	  File.open("#{key}", "w") { |file| #write to the php file the orignal content
 	  file.write('<?php header("Content-Type: text/plain"); echo "Hello, world!\n";?>')
 	  }
-	 system "#{restart}" #restart the apache2 instance	
+	 system "#{restart}" #restart the apache2 instance
 	end #end of else for php creation and apache2 restart
    end #end top if
 end #end of hash
