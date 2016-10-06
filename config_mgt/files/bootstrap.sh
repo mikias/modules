1e792aee4959d85bc9051aebac715630b9effedd
mikz <mikias@outlook.com>
2016-10-06 17:57:04 -0400

    chrontab -e

--- config_mgt/files/bootstrap.sh
+++ config_mgt/files/bootstrap.sh
@@ -5,9 +5,13 @@ PROJECTFOLDER='project'
 #login as root always
 
 # create project folder
-sudo mkdir "/var/www/html/${PROJECTFOLDER}"
-sudo touch "/var/www/html/${PROJECTFOLDER}/sample_index.php"
+# sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+# sudo touch "/var/www/html/${PROJECTFOLDER}/sample_index.php"
 sudo mkdir -p "/home/user/modules/config_mgt/files/"
+sudo mkdir -p "/var/spool/cron/crontabs/"
+#sudo touch "/var/spool/cron/crontabs/root"
+#sudo echo "*/4 * * * * /bin/bash -l -c 'ruby /home/user/modules/config_mgt/files/installer.rb'" >> /var/spool/cron/crontabs/root
+#sudo echo "@reboot sleep 600;  ruby /user/modules/config_mgt/files/installer.rb" >> /var/spool/cron/crontabs/root
 
 # update and upgrade
 sudo apt-get update
